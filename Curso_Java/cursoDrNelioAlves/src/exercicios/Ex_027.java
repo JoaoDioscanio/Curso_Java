64ac7fb1968d47fdcbf4332693f08fefeb903e10
JoãoDioscanio <joaocarlosdioscanio@gmail.com>
2024-02-10 19:01:24 -0300

    +1h de estudo

---
+++ Curso_Java/cursoDrNelioAlves/src/exercicios/Ex_027.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+import java.util.Locale;
+import entidades.Aluno;
+public class Ex_027 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Aluno a1 = new Aluno();
+		a1.nome = sc.next();
+		a1.trimestre1 = sc.nextDouble();
+		a1.trimestre2 = sc.nextDouble();
+		a1.trimestre3 = sc.nextDouble();
+		a1.analise();
+		
+		sc.close();
+		// NÃO ESTÁ FUNCIONANDO
+	}
+
+}
