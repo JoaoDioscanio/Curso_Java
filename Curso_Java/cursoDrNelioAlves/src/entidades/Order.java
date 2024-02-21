0050819cf7306fb992d21b085b57776ddc16340b
JoãoDioscanio <joaocarlosdioscanio@gmail.com>
2024-02-21 16:42:35 -0300

    +1h de estudo!
    Operações com datas e Enumerações

---
+++ Curso_Java/cursoDrNelioAlves/src/entidades/Order.java
@@ -0,0 +1,31 @@
+package entidades;
+
+import java.util.Date;
+
+import entidades.enums.OrderStatus;
+
+public class Order {
+	private long id;
+	private Date moment;
+	private OrderStatus status;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getMoment() {
+		return moment;
+	}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	
+}
