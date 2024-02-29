6e619305976e4be0bf075e67779ef4014aa16f82
JoãoDioscanio <joaocarlosdioscanio@gmail.com>
2024-02-29 16:12:15 -0300

    +1h de estudo
    Enumerações e Exemplo Worker

---
+++ Curso_Java/.metadata/.plugins/org.eclipse.core.resources/.history/a3/406f5b532fd7001e13dce5205130f4d4
@@ -0,0 +1,9 @@
+package entidades.enums;
+
+import java.util.Date;
+import entidades.Order;
+
+public enum OrderStatus {
+	PENDING_PAYMENT
+	Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+}
--- Curso_Java/cursoDrNelioAlves/src/entidades/enums/OrderStatus.java
+++ Curso_Java/cursoDrNelioAlves/src/entidades/enums/OrderStatus.java
@@ -1,9 +1,8 @@
 package entidades.enums;
 
-import java.util.Date;
-import entidades.Order;
-
 public enum OrderStatus {
-	PENDING_PAYMENT
-	Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+	PENDING_PAYMENT, 
+	PROCESSING, 
+	SHIPPED, 
+	DELIVERED;
 }
